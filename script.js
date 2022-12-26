74aa6fba6cc69131a1490cc58be2744a5875f143
Chris Choute <christopherchoute@gmail.com>
2022-12-26 14:29:34 -0500

    uh centered text

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
+h2.textContent = "Who I am and why I want to be a software Engineer";
  document.querySelector("body").appendChild(h2);
